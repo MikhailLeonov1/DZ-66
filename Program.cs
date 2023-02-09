c45f7f87a19d9c2b1052dc5b5da5bdd7fe5a4e41
user name <you@example.com>
2023-02-09 16:06:55 +0300

    дз номер 66

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число M:");
+int M = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма элементов в промежутке: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+
+GapNumberSum(M, N, 0);
